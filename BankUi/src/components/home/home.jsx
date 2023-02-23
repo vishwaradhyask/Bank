53cd0b61fae4fa4a3f6ecb91cd1e88786c0a52a5
Sangeetha S Hosmani <sangeetha.h@tataelxsi.co.in>
2023-02-24 00:45:22 +0530

    log-in page usename field completed

--- BankUi/src/components/home/home.jsx
+++ BankUi/src/components/home/home.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { FormattedMessage } from 'react-intl'
+import Bank_head from '../../images/bank_head.png'
+import User_png from '../../images/user .png'
 import './home.scss'
 class home extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      name:'vishwa'
+      name: 'vishwa'
     }
 
   }
@@ -17,17 +19,36 @@ class home extends Component {
     console.log('this.props:', this.state)
     return (
       <div className='home-page' >
-        this is home 
+        <div className='log-in'>
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <div className='bank-head-icon'>
+              <img src={Bank_head} alt="Bank_head" />
+            </div>
+          </div>
+          <div className='welcometxt'>
+            <h1 >Welcome to Gulbarga Bank</h1>
+          </div>
+          <div className='user-name'>
+            <div style={{ border: 'black solid 0.5px', width: '65%', display:'flex' }}>
+              <div className='username-icon'>
+                <img src={User_png} alt='User_png' />
+              </div>
+              <div className='usrname-input'>
+                <input type="text" placeholder='Enter User Name' />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
 }
 
 const mapStateToProps = (state) => ({
-app:   state.App,
-WindowManager: state.WindowManager,
-MultiLang: state.MultiLang,
-RecoveryMedia: state.RecoveryMedia,
+  app: state.App,
+  WindowManager: state.WindowManager,
+  MultiLang: state.MultiLang,
+  RecoveryMedia: state.RecoveryMedia,
 })
 
 const mapDispatchToProps = (dispatch) => ({
